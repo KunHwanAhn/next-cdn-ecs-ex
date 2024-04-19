b1bc567c7f66f87fd766ab9be38819124f587512
Conan Kunhwan Ahn <kunhwon@gmail.com>
2024-04-20 08:38:51 +0900

    불필요한 설정 제거

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const defaultConfig = {
   swcMinify: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    domains: [
-      'conan-dev.xyz',
-    ],
   },
 };
 
